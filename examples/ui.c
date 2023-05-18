75e6df0e2204e70b75210b462c27ab60a5b4abc6
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-05-18 04:49:46 -0500

    oops remove this

--- examples/ui.c
+++ examples/ui.c
@@ -126,9 +126,3 @@ void swap_buffers()
 	SDL_RenderCopy(renderer, texture, NULL, NULL);
 	SDL_RenderPresent(renderer);
 }
-
-#define KEY_UP 0xe000
-#define KEY_DOWN 0xe001
-#define KEY_LEFT 0xe002
-#define KEY_RIGHT 0xe003
-#define KEY_ESCAPE 0xe004
