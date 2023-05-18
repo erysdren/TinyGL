7f8a4e88bccbaf6ecdbcd6d0e063e678649ed1c8
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-05-17 22:44:02 -0500

    example makefile

--- examples/ui.h
+++ examples/ui.h
@@ -1,17 +1,16 @@
-/*
- * tk like ui
- */
-void draw( void );
-void idle( void );
+
+#include <GL/gl.h>
+
+void draw();
+void idle();
 GLenum key(int k, GLenum mask);
-void reshape( int width, int height );
-void init( void );
+void reshape(int width, int height);
+void init();
 int ui_loop(int argc, char **argv, const char *name);
-void tkSwapBuffers(void);
+void swap_buffers();
 
-#define KEY_UP     0xe000
-#define KEY_DOWN   0xe001
-#define KEY_LEFT   0xe002
-#define KEY_RIGHT  0xe003
+#define KEY_UP 0xe000
+#define KEY_DOWN 0xe001
+#define KEY_LEFT 0xe002
+#define KEY_RIGHT 0xe003
 #define KEY_ESCAPE 0xe004
-
