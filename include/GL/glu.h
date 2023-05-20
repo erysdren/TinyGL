1a96abd103ad1b209d7f315f806377d7154c4615
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-05-20 13:07:04 -0500

    glu.h whitespace and formatting

--- include/GL/glu.h
+++ include/GL/glu.h
@@ -1,15 +1,16 @@
 
 #include "gl.h"
 
-void gluPerspective( GLdouble fovy, GLdouble aspect,
-		     GLdouble zNear, GLdouble zFar );
+void gluPerspective(GLdouble fovy, GLdouble aspect,
+				GLdouble zNear, GLdouble zFar);
 
 void gluLookAt(GLdouble eyex, GLdouble eyey, GLdouble eyez,
-	GLdouble centerx, GLdouble centery, GLdouble centerz,
-	GLdouble upx, GLdouble upy, GLdouble upz);
+				GLdouble centerx, GLdouble centery, GLdouble centerz,
+				GLdouble upx, GLdouble upy, GLdouble upz);
 
-typedef struct {
-  int draw_style;
+typedef struct
+{
+	int draw_style;
 } GLUquadricObj;
 
 #define GLU_LINE 0
@@ -18,12 +19,12 @@ GLUquadricObj *gluNewQuadric(void);
 void gluQuadricDrawStyle(GLUquadricObj *obj, int style);
 
 void gluSphere(GLUquadricObj *qobj,
-               float radius,int slices,int stacks);
-void gluCylinder( GLUquadricObj *qobj,
-                  GLdouble baseRadius, GLdouble topRadius, GLdouble height,
-                  GLint slices, GLint stacks );
-void gluDisk( GLUquadricObj *qobj,
-              GLdouble innerRadius, GLdouble outerRadius,
-              GLint slices, GLint loops );
+				float radius,int slices,int stacks);
+void gluCylinder(GLUquadricObj *qobj,
+				GLdouble baseRadius, GLdouble topRadius, GLdouble height,
+				GLint slices, GLint stacks);
+void gluDisk(GLUquadricObj *qobj,
+				GLdouble innerRadius, GLdouble outerRadius,
+				GLint slices, GLint loops);
 
 void drawTorus(float rc, int numc, float rt, int numt);
