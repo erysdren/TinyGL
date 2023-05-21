5d63add3ff09d8081e0f6f1fda106ed73bcd0e2b
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-05-21 13:52:50 -0500

    fix haiku i586 cross compiler script

--- build-haiku-i586.sh
+++ build-haiku-i586.sh
@@ -5,6 +5,12 @@ then
 	mkdir cmake-build-haiku-i586
 fi
 
+# oh, a hack!
+TEMP=${SDL2_DIR}
+export SDL2_DIR=/usr/lib32/cmake/SDL2/
+
 cd cmake-build-haiku-i586
 cmake -D CMAKE_TOOLCHAIN_FILE=cmake/haiku-i586.cmake ..
 make
+
+export SDL2_DIR=${TEMP}
